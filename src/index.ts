0d2b5e47786df3190bec2de5b04979ffdcb339c3
Ryeowon Hong <ryeowon13@gmail.com>
2022-01-22 20:01:10 +0900

    add tsc-watch

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "ryeowon",
+    age = 24,
+    gender = "female"
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+}
+
+console.log(sayHi(name, 22, gender));
+
+export { };
